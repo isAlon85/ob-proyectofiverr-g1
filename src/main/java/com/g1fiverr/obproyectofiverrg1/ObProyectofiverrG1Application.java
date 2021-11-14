952329429e79768afce85ac1f965cf5a35957068
isAlon85 <ianstry85@gmail.com>
2021-11-14 13:01:38 +0100

    Initial commit

---
+++ src/main/java/com/g1fiverr/obproyectofiverrg1/ObProyectofiverrG1Application.java
@@ -0,0 +1,13 @@
+package com.g1fiverr.obproyectofiverrg1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ObProyectofiverrG1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ObProyectofiverrG1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/g1fiverr/obproyectofiverrg1/ObProyectofiverrG1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.g1fiverr.obproyectofiverrg1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ObProyectofiverrG1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
